94294f54e50bd0f14904f7046aa37e91b6eaef4c
arasruislam <arasru01@gmail.com>
2024-07-12 07:15:42 +0600

    create model for book

--- books/models.py
+++ books/models.py
@@ -1,5 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Book(models.Model):
-    pass
+    title = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    publication_date = models.DateField(auto_now_add=True, blank=True, null=True)
+    image = models.ImageField(upload_to="book_images/", blank=True, null=True)
+    borrowing_price = models.DecimalField(
+        max_digits=5, decimal_places=2, blank=True, null=True
+    )
+    category = models.ManyToManyField(Category)
+
+    def __str__(self) -> str:
+        return self.title
